861a224f455351ebabdea42077fc69edc5f3aac7
Felipe Cano <felipe@acid.cl>
2015-10-05 10:20:29 -0300

    Add admin view for Khipu payments

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,6 +1,7 @@
 module Spree
     Payment.class_eval do
         scope :from_khipu, -> { joins(:payment_method).where(spree_payment_methods: {type: Spree::Gateway::KhipuGateway.to_s}) }
+        has_one :khipu_payment_receipt, foreign_key: 'transaction_id', primary_key: 'identifier'
 
         def khipu_payment_method?
           payment_method && payment_method.type == Spree::Gateway::KhipuGateway.to_s
